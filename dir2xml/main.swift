fb977cd599b3dd240e76fde6a5a1c9fe2a0b95a7
Paul Sobolik <psobolik@zoho.com>
2016-08-14 09:54:52 -0400

    Initial Commit

---
+++ dir2xml/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  dir2xml
+//
+//  Created by Paul Sobolik on 8/14/16.
+//  Copyright © 2016 Paul Sobolik. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
